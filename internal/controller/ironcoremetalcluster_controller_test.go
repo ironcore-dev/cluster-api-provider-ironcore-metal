81364d798b4caaca41f1be5be77e3f900fda9231
Anton Paulovich <a.paulovich@sap.com>
2026-02-05 15:12:15 +0100

    Cluster controller integration tests

--- internal/controller/ironcoremetalcluster_controller_test.go
+++ internal/controller/ironcoremetalcluster_controller_test.go
@@ -5,67 +5,227 @@ package controller
 
 import (
 	"context"
+	"time"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
-	"k8s.io/apimachinery/pkg/api/errors"
-	"k8s.io/apimachinery/pkg/types"
-	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-
+	"github.com/google/uuid"
 	infrav1 "github.com/ironcore-dev/cluster-api-provider-ironcore-metal/api/v1alpha1"
+	corev1 "k8s.io/api/core/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+	clusterv1 "sigs.k8s.io/cluster-api/api/v1beta1"
+	"sigs.k8s.io/cluster-api/util/conditions"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	ctrlutil "sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 )
 
 var _ = Describe("IroncoreMetalCluster Controller", func() {
-	Context("When reconciling a resource", func() {
-		const resourceName = "test-resource"
 
-		ctx := context.Background()
+	var (
+		ctx                context.Context
+		namespace          string
+		clusterName        string
+		typeNamespacedName types.NamespacedName
+		ironcoreCluster    *infrav1.IroncoreMetalCluster
+		capiCluster        *clusterv1.Cluster
+		reconciler         *IroncoreMetalClusterReconciler
+	)
 
-		typeNamespacedName := types.NamespacedName{
-			Name:      resourceName,
-			Namespace: "default", // TODO(user):Modify as needed
+	BeforeEach(func() {
+		ctx = context.Background()
+		namespace = "default"
+		clusterName = "test-cluster-" + uuid.NewString()
+
+		typeNamespacedName = types.NamespacedName{
+			Name:      clusterName,
+			Namespace: namespace,
 		}
-		ironcoremetalcluster := &infrav1.IroncoreMetalCluster{}
 
-		BeforeEach(func() {
-			By("creating the custom resource for the Kind IroncoreMetalCluster")
-			err := k8sClient.Get(ctx, typeNamespacedName, ironcoremetalcluster)
-			if err != nil && errors.IsNotFound(err) {
-				resource := &infrav1.IroncoreMetalCluster{
-					ObjectMeta: metav1.ObjectMeta{
-						Name:      resourceName,
-						Namespace: "default",
+		reconciler = &IroncoreMetalClusterReconciler{
+			Client: k8sClient,
+			Scheme: k8sClient.Scheme(),
+		}
+
+		capiCluster = &clusterv1.Cluster{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      clusterName,
+				Namespace: namespace,
+			},
+		}
+
+		ctrlutil.AddFinalizer(capiCluster, "cluster.cluster.x-k8s.io")
+		Expect(k8sClient.Create(ctx, capiCluster)).To(Succeed())
+
+		ironcoreCluster = &infrav1.IroncoreMetalCluster{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      clusterName,
+				Namespace: namespace,
+				OwnerReferences: []metav1.OwnerReference{
+					{
+						APIVersion: clusterv1.GroupVersion.String(),
+						Kind:       "Cluster",
+						Name:       capiCluster.Name,
+						UID:        capiCluster.UID,
 					},
-					// TODO(user): Specify other spec details if needed.
-				}
-				Expect(k8sClient.Create(ctx, resource)).To(Succeed())
-			}
-		})
+				},
+			},
+		}
+	})
 
-		AfterEach(func() {
-			// TODO(user): Cleanup logic after each test, like removing the resource instance.
-			resource := &infrav1.IroncoreMetalCluster{}
-			err := k8sClient.Get(ctx, typeNamespacedName, resource)
-			Expect(err).NotTo(HaveOccurred())
+	AfterEach(func() {
+		if ironcoreCluster != nil {
+			err := k8sClient.Delete(ctx, ironcoreCluster)
+			Expect(client.IgnoreNotFound(err)).To(Succeed())
+		}
 
-			By("Cleanup the specific resource instance IroncoreMetalCluster")
-			Expect(k8sClient.Delete(ctx, resource)).To(Succeed())
-		})
-		It("should successfully reconcile the resource", func() {
-			By("Reconciling the created resource")
-			controllerReconciler := &IroncoreMetalClusterReconciler{
-				Client: k8sClient,
-				Scheme: k8sClient.Scheme(),
+		if capiCluster != nil {
+			key := client.ObjectKeyFromObject(capiCluster)
+			if err := k8sClient.Get(ctx, key, capiCluster); err != nil {
+				return
 			}
 
-			_, err := controllerReconciler.Reconcile(ctx, reconcile.Request{
-				NamespacedName: typeNamespacedName,
-			})
+			if ctrlutil.RemoveFinalizer(capiCluster, "cluster.cluster.x-k8s.io") {
+				Expect(k8sClient.Update(ctx, capiCluster)).To(Succeed())
+			}
+
+			err := k8sClient.Delete(ctx, capiCluster)
+			Expect(client.IgnoreNotFound(err)).To(Succeed())
+		}
+	})
+
+	Context("When reconciling normal", func() {
+		It("Should set the Finalizer and mark the status and condition as Ready", func() {
+			Expect(k8sClient.Create(ctx, ironcoreCluster)).To(Succeed())
+
+			_, err := reconciler.Reconcile(ctx, reconcile.Request{NamespacedName: typeNamespacedName})
 			Expect(err).NotTo(HaveOccurred())
-			// TODO(user): Add more specific assertions depending on your controller's reconciliation logic.
-			// Example: If you expect a certain status condition after reconciliation, verify it here.
+
+			err = k8sClient.Get(ctx, typeNamespacedName, ironcoreCluster)
+			Expect(err).NotTo(HaveOccurred())
+
+			By("Verifying the Finalizer is added")
+			Expect(ironcoreCluster.Finalizers).To(ContainElement(infrav1.ClusterFinalizer))
+
+			By("Verifying the Status is Ready")
+			Expect(ironcoreCluster.Status.Ready).To(BeTrue())
+
+			By("Verifying the ClusterReady condition")
+			condition := conditions.Get(ironcoreCluster, infrav1.IroncoreMetalClusterReady)
+			Expect(condition).NotTo(BeNil())
+			Expect(condition.Status).To(Equal(corev1.ConditionTrue))
+		})
+
+		It("Should not reconcile if IroncoreMetalCluster has no OwnerReference to Cluster", func() {
+			ironcoreCluster.OwnerReferences = []metav1.OwnerReference{}
+			Expect(k8sClient.Create(ctx, ironcoreCluster)).To(Succeed())
+
+			_, err := reconciler.Reconcile(ctx, reconcile.Request{NamespacedName: typeNamespacedName})
+			Expect(err).NotTo(HaveOccurred())
+
+			Expect(k8sClient.Get(ctx, typeNamespacedName, ironcoreCluster)).To(Succeed())
+
+			By("Ensuring Finalizer is NOT added")
+			Expect(ironcoreCluster.Finalizers).NotTo(ContainElement(infrav1.ClusterFinalizer))
+
+			By("Ensuring Status.Ready is NOT set")
+			Expect(ironcoreCluster.Status.Ready).To(BeFalse())
+
+			By("Ensuring the ClusterReady condition is NOT set")
+			condition := conditions.Get(ironcoreCluster, infrav1.IroncoreMetalClusterReady)
+			Expect(condition).To(BeNil())
+		})
+
+		It("Should not reconcile if IroncoreMetalCluster is paused", func() {
+			if ironcoreCluster.Annotations == nil {
+				ironcoreCluster.Annotations = map[string]string{}
+			}
+			ironcoreCluster.Annotations[clusterv1.PausedAnnotation] = "true"
+			Expect(k8sClient.Create(ctx, ironcoreCluster)).To(Succeed())
+
+			_, err := reconciler.Reconcile(ctx, reconcile.Request{NamespacedName: typeNamespacedName})
+			Expect(err).NotTo(HaveOccurred())
+
+			Expect(k8sClient.Get(ctx, typeNamespacedName, ironcoreCluster)).To(Succeed())
+
+			By("Ensuring Finalizer is NOT added")
+			Expect(ironcoreCluster.Finalizers).NotTo(ContainElement(infrav1.ClusterFinalizer))
+
+			By("Ensuring Status.Ready is NOT set")
+			Expect(ironcoreCluster.Status.Ready).To(BeFalse())
+
+			By("Ensuring the ClusterReady condition is NOT set")
+			condition := conditions.Get(ironcoreCluster, infrav1.IroncoreMetalClusterReady)
+			Expect(condition).To(BeNil())
+		})
+	})
+
+	Context("When reconciling a delete", func() {
+		It("should NOT remove finalizer if owning CAPI Cluster is NOT deleted", func() {
+			ironcoreCluster.Finalizers = []string{infrav1.ClusterFinalizer}
+			Expect(k8sClient.Create(ctx, ironcoreCluster)).To(Succeed())
+
+			Expect(k8sClient.Delete(ctx, ironcoreCluster)).To(Succeed())
+
+			_, err := reconciler.Reconcile(ctx, reconcile.Request{NamespacedName: typeNamespacedName})
+			Expect(err).NotTo(HaveOccurred())
+
+			Expect(k8sClient.Get(ctx, typeNamespacedName, ironcoreCluster)).To(Succeed())
+			Expect(ironcoreCluster.Finalizers).To(ContainElement(infrav1.ClusterFinalizer))
+		})
+
+		It("should NOT remove finalizer if child Machines exist", func() {
+			ironcoreCluster.Finalizers = []string{infrav1.ClusterFinalizer}
+			Expect(k8sClient.Create(ctx, ironcoreCluster)).To(Succeed())
+
+			machine := &infrav1.IroncoreMetalMachine{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "machine-" + clusterName,
+					Namespace: namespace,
+					Labels:    map[string]string{clusterv1.ClusterNameLabel: clusterName},
+				},
+			}
+			Expect(k8sClient.Create(ctx, machine)).To(Succeed())
+
+			defer func() {
+				_ = k8sClient.Delete(ctx, machine)
+			}()
+
+			Expect(k8sClient.Delete(ctx, capiCluster)).To(Succeed())
+			Expect(k8sClient.Delete(ctx, ironcoreCluster)).To(Succeed())
+
+			result, err := reconciler.Reconcile(ctx, reconcile.Request{NamespacedName: typeNamespacedName})
+			Expect(err).NotTo(HaveOccurred())
+
+			By("Verifying Requeue is requested")
+			Expect(result.RequeueAfter).To(BeNumerically("==", infrav1.DefaultReconcilerRequeue))
+
+			By("Verifying Finalizer is STILL present")
+			Expect(k8sClient.Get(ctx, typeNamespacedName, ironcoreCluster)).To(Succeed())
+			Expect(ironcoreCluster.Finalizers).To(ContainElement(infrav1.ClusterFinalizer))
+		})
+
+		It("should remove finalizer if NO child Machines exist", func() {
+			ironcoreCluster.Finalizers = []string{infrav1.ClusterFinalizer}
+			Expect(k8sClient.Create(ctx, ironcoreCluster)).To(Succeed())
+
+			Expect(k8sClient.Delete(ctx, capiCluster)).To(Succeed())
+			Expect(k8sClient.Delete(ctx, ironcoreCluster)).To(Succeed())
+
+			result, err := reconciler.Reconcile(ctx, reconcile.Request{NamespacedName: typeNamespacedName})
+			Expect(err).NotTo(HaveOccurred())
+
+			By("Verifying NO Requeue is requested")
+			Expect(result.RequeueAfter).To(Equal(time.Duration(0)))
+
+			By("Verifying Finalizer is REMOVED")
+			err = k8sClient.Get(ctx, typeNamespacedName, ironcoreCluster)
+			if !apierrors.IsNotFound(err) {
+				Expect(ironcoreCluster.Finalizers).NotTo(ContainElement(infrav1.ClusterFinalizer))
+			}
 		})
 	})
 })
